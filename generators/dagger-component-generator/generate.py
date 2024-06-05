3a47f56c6431b40b1fdc388d39085603545e8daa
fathonyfath <4829378+fathonyfath@users.noreply.github.com>
2024-06-05 23:54:19 +0900

    Combine generators

--- generators/dagger-component-generator/generate.py
+++ generators/dagger-component-generator/generate.py
@@ -1,15 +1,16 @@
 import cookiecutter.main as ccmain
+import os
 
 
-def generate_component(module: str, module_name: str, package_name: str):
+def generate_component(module: str, module_name: str):
     ccmain.cookiecutter(
         template="component",
         no_input=True,
         extra_context={
             "module": module,
             "module_name": module_name,
-            "package_name": package_name,
         },
+        output_dir="./result",
     )
 
 
@@ -21,6 +22,7 @@ def generate_activity(package_name: str, activity_name: str):
             "package_name": package_name,
             "activity_name": activity_name,
         },
+        output_dir="./result/activities",
     )
 
 
@@ -31,11 +33,24 @@ def generate_layout(activity_name: str):
         extra_context={
             "activity_name": activity_name,
         },
+        output_dir="./result/layouts",
     )
 
 
-generate_component(
+def generate_module(module: str, module_name: str, activity_names: list[str]):
+    assert len(activity_names) > 0, "Activity names should not be empty."
+    generate_component(module=module, module_name=module_name)
+    for name in activity_names:
+        generate_activity(package_name=module_name, activity_name=name)
+        generate_layout(activity_name=name)
+
+
+generate_module(
     module="FooBar",
     module_name="foobar",
-    package_name="foobar",
+    activity_names=[
+        "Page1",
+        "Page2",
+        "Page3",
+    ],
 )
