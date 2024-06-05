d0cbb12003ee65a59ffc54dbce2ecf96df21f3e4
fathonyfath <4829378+fathonyfath@users.noreply.github.com>
2024-06-05 23:39:57 +0900

    Basic layout

--- generators/dagger-component-generator/generate.py
+++ generators/dagger-component-generator/generate.py
@@ -1,27 +1,41 @@
 import cookiecutter.main as ccmain
 
 
-def generate_component():
+def generate_component(module: str, module_name: str, package_name: str):
     ccmain.cookiecutter(
-        "component",
+        template="component",
         no_input=True,
         extra_context={
-            "module": "Random",
-            "module_name": "random",
-            "package_name": "random",
+            "module": module,
+            "module_name": module_name,
+            "package_name": package_name,
         },
     )
 
 
-def generate_activity():
+def generate_activity(package_name: str, activity_name: str):
     ccmain.cookiecutter(
-        "activity",
+        template="activity",
         no_input=True,
         extra_context={
-            "package_name": "random",
-            "activity_name": "Random",
+            "package_name": package_name,
+            "activity_name": activity_name,
         },
     )
 
 
-generate_activity()
+def generate_layout(activity_name: str):
+    ccmain.cookiecutter(
+        template="layout",
+        no_input=True,
+        extra_context={
+            "activity_name": activity_name,
+        },
+    )
+
+
+generate_component(
+    module="FooBar",
+    module_name="foobar",
+    package_name="foobar",
+)
